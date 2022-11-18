590dcb862ee6f20a970df5ed023f5ad7633904b2
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-11-18 03:05:01 -0300

    Navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import "./style.css";
 
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
+import { FaBars, FaTimes } from "react-icons/fa";
+
 export const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
         <Link to="/">
             <h1>Portfolio.</h1>
         </Link>
 
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -24,6 +29,13 @@ export const Navbar = () => {
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? 
+            (<FaTimes size={20} style={{color: "#fff"}}/> ) 
+            : 
+            (<FaBars size={20} style={{color: "#fff"}}/>)}
+            
+        </div>
     </div>
   )
 }
