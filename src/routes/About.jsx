89cb488d426298aa8d2cf4a537d614d6c559ec21
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2022-12-02 00:11:14 -0300

    About page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
 import { Footer } from '../components/Footer/Footer'
 import { HeroSectionGeneral } from '../components/HeroSectionGeneral/HeroSectionGeneral'
+import { AboutContent } from '../components/About/AboutContent'
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <HeroSectionGeneral heading="ABOUT" text="I'm a friendly front-end developer"/>
+      <AboutContent />
       <Footer />
     </div>
   )
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
 import { HeroSection } from '../components/HeroSection/HeroSection'
 import { Footer } from '../components/Footer/Footer'
+import { Work } from '../components/Work/Work'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Work />
       <Footer />
     </div>
   )
