9e85d348af2c16fe28f2934e314eb786914d9f95
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2022-12-02 00:25:40 -0300

    Remove projects from the home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
 import { HeroSection } from '../components/HeroSection/HeroSection'
 import { Footer } from '../components/Footer/Footer'
-import { Work } from '../components/Work/Work'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <Work />
       <Footer />
     </div>
   )
